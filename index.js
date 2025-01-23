c9ff352637768739a08096fee999a0a935c9971f
Nik <nikolaitomov25@gmail.com>
2025-01-23 20:10:52 +0200

    added a semiworking apple mechanic needs alittle fixing on the x and y random positions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let xPos = 0;
 let yPos = 0;
 let direction = "ArrowRight";
 let snakeSpeed = 500;
+let currApples = 0;
 
 const maxX = window.innerWidth;
 const maxY = window.innerHeight;
@@ -87,13 +88,39 @@ function getDir() {
 
 function populateApples() {
     let apple = document.getElementById("apple");
-    console.log((apple.style.left == snakeChar.style.left) && (apple.style.top == snakeChar.style.top));
-    if ((apple.style.left == snakeChar.style.left) && (apple.style.top == snakeChar.style.top)) {
-        apple.remove();
-        let newApple = document.createElement("div");
-        newApple.id = "apple";
-        newApple.style.left = Math.floor(Math.random * (maxX - step));
-        newApple.style.top = Math.floor (Math.random * (maxY - step));
-        gameArea.appendChild(newApple);
+    if (currApples == 0) {
+        makeApple();
+        currApples++;
     }
+    
+    if ((snakeChar.style.left === apple.style.left) && (snakeChar.style.top === apple.style.top)) {
+        console.log("Hello World");
+        apple.remove();
+        currApples--;
+    }
+    // console.log(`Char x: ${snakeChar.style.left} y: ${snakeChar.style.top}`)
+    // console.log(`Apple x: ${apple.style.left} y: ${apple.style.top}`)
+}
+
+function makeApple() {
+    let newApple = document.createElement("div");
+    newApple.id = "apple";
+    let randX = getRandomMultipleOf48(48, maxX);
+    let randY = getRandomMultipleOf48(48, maxY);
+    newApple.style.left = `${randX}px`;
+    newApple.style.top = `${randY}px`;
+    gameArea.appendChild(newApple);
+}
+
+function getRandomMultipleOf48(min, max) {
+    min = Math.ceil(min / 48) * 48; // Round up to the nearest multiple of 48
+    max = Math.floor(max / 48) * 48; // Round down to the nearest multiple of 48;
+
+    if (min > max) {
+        throw new Error("No mulitple of 48 in the given range.");
+    }
+
+    let rand = Math.floor(Math.random() * (max - min) / 48 + 1);
+
+    return min + rand * 48;
 }
